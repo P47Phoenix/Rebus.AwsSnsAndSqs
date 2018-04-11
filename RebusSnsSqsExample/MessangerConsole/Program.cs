1d53475367da84d53dca7b0c283e86d41db0cd8e
P47Phoenix <michaelconne@gmail.com>
2018-04-10 21:10:55 -0500

    Added a simple example and a readme

--- RebusSnsSqsExample/MessangerConsole/Program.cs
+++ RebusSnsSqsExample/MessangerConsole/Program.cs
@@ -1,59 +1,4 @@
-# Rebus.AwsSnsAndSqs
-Implement aws sns and sqs provider for rebus
-
-## Contract based pubsub
-
-For example this following contract
-```
-using System;
-
-namespace Topic.Contracts
-{
-    public class MessengerMessage
-    {
-        public string Message { get; set; }
-
-        public DateTime CreateDateTime { get; set; }
-        public string Sender { get; set; }
-    }
-}
-```
-
-Will create a topic like
-```
-Topic_Contracts_MessengerMessage--Topic_Contracts
-```
-
-In this example of pub sub the client and the worker is the same.
-Each instance will create queue using the machine name and pid of the process.
-```
-var queueName = $"{Environment.MachineName}_{Process.GetCurrentProcess().Id}".ToLowerInvariant();
-```
-Next we should subscribe to the topic.
-```
-await bus.Subscribe<MessengerMessage>();
-```
-Finally we just publish new messages we get onto the topic
-```
-var line = String.Empty;
-do
-{
-    Console.Write("message:");
-    line = Console.ReadLine();
-
-    await bus.Publish(new MessengerMessage
-    {
-        CreateDateTime = DateTime.Now,
-        Message = line,
-        Sender = queueName
-    });
-}
-while (string.IsNullOrWhiteSpace(line) == false);
-```
-
-Full example
-```
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Rebus.Activation;
@@ -128,4 +73,3 @@ namespace MessangerConsole
         }
     }
 }
-```
---
+++ RebusSnsSqsExample/Topic.Contracts/MessengerMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Topic.Contracts
+{
+    public class MessengerMessage
+    {
+        public string Message { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+        public string Sender { get; set; }
+    }
+}
